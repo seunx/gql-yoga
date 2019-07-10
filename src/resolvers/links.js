54858451b1ea15e3a9d24de8496a0dab209f7524
Seun <12373359+NatureBoi@users.noreply.github.com>
2019-07-10 17:53:26 -0400

    Implemented two subscription services

--- src/resolvers/links.js
+++ src/resolvers/links.js
@@ -11,6 +11,35 @@ const newLink = {
 	}
 };
 
+const vote = async (root, args, ctx, info) => {
+	const userId = getUserId(ctx);
+
+	const linkExists = await ctx.prisma.$exists.vote({
+		user: { id: userId },
+		link: { id: args.linkId }
+	});
+
+	if (linkExists) {
+		throw new Error(`Already voted for link: ${args.linkId}`);
+	}
+
+	return ctx.prisma.createVote({
+		user: { connect: { id: userId } },
+		link: { connect: { id: args.linkId } }
+	});
+};
+
+const newVoteSubscribe = (root, args, ctx, info) => {
+	return ctx.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node();
+};
+
+const newVote = {
+	subscribe: newVoteSubscribe,
+	resolve: payload => {
+		return payload;
+	}
+};
+
 module.exports = {
 	Query: {
 		feed: (root, args, ctx, info) => {
@@ -25,14 +54,27 @@ module.exports = {
 				description: args.description,
 				postedBy: { connect: { id: userId } }
 			});
-		}
+		},
+		vote
 	},
 	Subscription: {
-		newLink
+		newLink,
+		newVote
 	},
 	Link: {
 		postedBy(parent, args, ctx) {
 			return ctx.prisma.link({ id: parent.id }).postedBy();
+		},
+		votes(parent, args, ctx) {
+			return ctx.prisma.link({ id: parent.id }).votes();
+		}
+	},
+	Vote: {
+		link(parent, args, ctx) {
+			return ctx.prisma.vote({ id: parent.id }).link();
+		},
+		user(parent, args, ctx) {
+			return ctx.prisma.vote({ id: parent.id }).user();
 		}
 	}
 };
